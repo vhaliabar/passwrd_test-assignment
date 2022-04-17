cff88d61e5b664227f0d33ad71ae242a17bef5ae
vhaliabar <vhaliabar@yahoo.com>
2022-04-17 14:45:24 +0300

    initial commit

---
+++ pass_generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import string
+import random
+
+## characters to generate password from
+characters = list(string.ascii_letters + string.digits + string.punctuation)
+
+def generate_random_password():
+	## shuffling the characters
+	random.shuffle(characters)
+	
+	## picking random characters from the list
+	password = []
+	for i in range(14):
+		password.append(random.choice(characters))
+    
+	## shuffling the resultant password
+	random.shuffle(password)
+
+	## converting the list to string
+	## printing the list
+	print("".join(password))
+
+
+
+## invoking the function      
+if __name__ == '__main__':
+    generate_random_password()
---
+++ pass_validator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import string
+import sys
+test_passwrd = sys.argv[1]
+
+def password_check(passwd):
+    val = True
+    
+    if not any(char.isupper() for char in passwd) or not any(char.islower() for char in passwd):
+        print('- Password must contain both lowercase and uppercase letters')
+        val = False     
+             
+    if not any(char.isdigit() for char in passwd):
+        print('- Password must contain digits')
+        val = False
+          
+    if not any(char in string.punctuation for char in passwd):
+        print('- Password must contain at least one punctuation character(!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~)')
+        val = False
+    
+    if len(passwd) <= 14:
+        print('- Password should be at least 14 characters long')
+        val = False
+    
+    if val:
+        print('Strong password')
+  
+# Driver Code        
+if __name__ == '__main__':
+    password_check(test_passwrd)
